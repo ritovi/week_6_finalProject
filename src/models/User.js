e5b12e178eb92ec749696f3c19649a9cbe1bd132
ritovi <149262897+ritovi@users.noreply.github.com>
2024-08-20 16:32:31 -0700

    day1

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+const sequelize = require("../utils/connection");
+const {DataTypes} = require("sequelize");
+const bcrypt = require("bcrypt")
+
+const User = sequelize.define("user",{
+    firstName : {
+        type: DataTypes.STRING,
+        allowNull:false,
+    },
+    lastName : {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        unique:true,
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull:false,
+    },
+    phone :{
+        type: DataTypes.STRING,
+        allowNull:false,
+    }
+});
+
+User.beforeCreate(async(user)=>{
+    const password = user.password;
+    const hashedPassword  = await bcrypt.hash(password,10);
+
+    user.password = hashedPassword;
+})
+
+
+module.exports = User;
